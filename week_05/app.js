fcfa439c4e930a6435558e03e90bc024577f94c2
Floyd_Carlo <codeit69420@gmail.com>
2025-02-17 06:47:13 +0000

    img

--- week_05/app.js
+++ week_05/app.js
@@ -1,10 +1,10 @@
 const products = [
-    { id: 1, name: "Product 1", price: 19.99, image: "watch.png", width:200, height:200 },
-    { id: 2, name: "Product 2", price: 29.99, image: "perfume.png", width:200, height:200},
-    { id: 3, name: "Product 3", price: 39.99, image: "shoes.png", width:200, height:200 },
-    { id: 4, name: "Product 4", price: 49.99, image: "phone.png", width:200, height:200 },
-    { id: 5, name: "Product 5", price: 49.99, image: "laptop.png", width:200, height:200 },
-    { id: 6, name: "Product 6", price: 49.99, image: "bottle.png", width:200, height:200 },
+    { id: 1, name: "Product 1", price: 19.99, image: "images/watch.png", width:200, height:200 },
+    { id: 2, name: "Product 2", price: 29.99, image: "images/perfume.png", width:200, height:200},
+    { id: 3, name: "Product 3", price: 39.99, image: "images/shoes.png", width:200, height:200 },
+    { id: 4, name: "Product 4", price: 49.99, image: "images/phone.png", width:200, height:200 },
+    { id: 5, name: "Product 5", price: 49.99, image: "images/laptop.png", width:200, height:200 },
+    { id: 6, name: "Product 6", price: 49.99, image: "images/bottle.png", width:200, height:200 },
   ]
   
   const cart = []
